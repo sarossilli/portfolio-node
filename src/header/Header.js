56e6a3ba455f9ce5d8fba8423acffb76400f93b5
Sam Rossilli <sarossilli@gmail.com>
2021-10-24 20:20:40 -0600

    added header with bootstrap

---
+++ src/header/Header.js
@@ -0,0 +1,32 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import logo from '../logo.svg'
+function Header() {
+  return (
+    <div className="Header">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Sam Rossilli
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+
+
+  );
+}
+
+export default Header;
